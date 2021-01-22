a746786f2f681cd974165a43a5b137a315f33057
Rediet8abere <redietaberegesesse@gmail.com>
2021-01-21 17:07:43 -0800

    solutions

---
+++ src/index.js
@@ -0,0 +1,115 @@
+
+function capitalizeFirst(str) {
+  // capitalize first letter of a word
+  const newStr = str.charAt(0).toUpperCase() + str.slice(1)
+  return newStr
+}
+
+console.log(capitalizeFirst('hello'))
+
+
+function allCaps(str) {
+  return str.toUpperCase()
+}
+
+console.log(allCaps('hello'))
+
+
+function capitalizeWords(str) {
+  const strList = str.split(' ')
+  let newStr = ''
+  for (let i=0; i < strList.length; i++) {
+    newStr += capitalizeFirst(strList[i]) + ' '
+  }
+  return newStr
+}
+
+console.log(capitalizeWords('we do all things right'))
+
+function capitalizeHeadLine(str) {
+  const prop = new Set(['the', 'in', 'a', 'an', 'and', 'but', 'for', 'at', 'by', 'from'])
+  const strList = str.split(' ')
+  let newStr = ''
+
+  for (let i=0; i < strList.length; i++) {
+    if (prop.has(strList[i])) {
+      newStr += strList[i] + ' '
+    } else {
+      newStr += capitalizeFirst(strList[i]) + ' '
+    }
+
+  }
+  return newStr
+}
+
+console.log(capitalizeHeadLine('we do all things in the name of god by the will of people'))
+
+
+function removeExtraSpace(str){
+  const strTri = str.trim()
+  const strList = strTri.split(' ')
+  let newStr = ''
+  for (let i=0; i < strList.length; i++) {
+    if (strList[i] != '') {
+      newStr += strList[i] + ' '
+    } else {
+      newStr += strList[i]
+    }
+  }
+  return newStr
+
+}
+
+console.log(removeExtraSpace("   Hello    world!   "))
+
+
+function kebobCase(str) {
+  const strTri = str.trim()
+  const strList = strTri.split(" ")
+  return strList.join('-')
+}
+
+console.log(kebobCase(" Hello world "))
+
+function snakeCase(str) {
+  const strTri = str.trim()
+  const strList = strTri.split(" ")
+  return strList.join('_')
+}
+
+console.log(snakeCase(" what the heck "))
+
+
+function camelCase(str) {
+  const strTri = str.trim()
+  const strList = strTri.split(" ")
+  let newStr = ''
+  for (let i=0; i<strList.length; i++) {
+    if (i==0) {
+      newStr += strList[i].toLowerCase()
+    } else {
+      newStr += capitalizeFirst(strList[0])
+    }
+  }
+  return newStr
+}
+
+console.log(camelCase('Camel Case'))
+
+
+function shift(str, num) {
+  let newStr = str.slice(num) + str.slice(0, num)
+  return newStr
+}
+
+console.log(shift('hello world', 2))
+
+
+function makeHashTag(str) {
+  const strTri = capitalizeWords(str.trim())
+  const strList = strTri.split(" ")
+
+  return '#' + strList.join('')
+}
+
+console.log(makeHashTag('go for it'))
